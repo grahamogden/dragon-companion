1728326c4e23c8e7112a0f21b006e3c6e9563285
grahamogden <gjogden387@gmail.com>
2018-06-23 15:02:27 +0100

    - Added log in/out functionality, as per CakePHP auth tutorial

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -53,6 +53,26 @@ class AppController extends Controller
          */
         //$this->loadComponent('Security');
         $this->loadComponent('Flash');
+        $this->loadComponent('Auth', [
+            'authenticate' => [
+                'Form' => [
+                    'fields' => [
+                        'username' => 'username',
+                        'password' => 'password'
+                    ]
+                ]
+            ],
+            'loginAction' => [
+                'controller' => 'Users',
+                'action' => 'login'
+            ],
+             // If unauthorized, return them to page they were just on
+            'unauthorizedRedirect' => $this->referer()
+        ]);
+
+        // Allow the display action so our PagesController
+        // continues to work. Also enable the read only actions.
+        $this->Auth->allow(['display', 'view', 'index']);
     }
 
 }
--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use App\Controller\AppController;
@@ -16,7 +17,7 @@ class UsersController extends AppController
     public function initialize()
     {
         parent::initialize();
-//        $this->Auth->allow(['logout', 'add']);
+        $this->Auth->allow(['logout', 'add']);
     }
 
     /**
@@ -110,4 +111,23 @@ class UsersController extends AppController
 
         return $this->redirect(['action' => 'index']);
     }
+
+    public function login()
+    {
+        if ($this->request->is('post')) {
+            $user = $this->Auth->identify();
+            if ($user) {
+                $this->Auth->setUser($user);
+                return $this->redirect($this->Auth->redirectUrl());
+            }
+            $this->Flash->error('Your username or password is incorrect.');
+        }
+    }
+
+    public function logout()
+    {
+        $this->Flash->success('You are now logged out.');
+        return $this->redirect($this->Auth->logout());
+    }
+
 }
