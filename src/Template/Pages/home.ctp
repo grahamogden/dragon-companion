b69c23f7f52a11908988edcfe03c993342a76853
Unknown <gjogden387@gmail.com>
2019-05-18 22:43:49 +0100

    Minor changes

--- src/Template/Pages/home.ctp
+++ src/Template/Pages/home.ctp
@@ -19,7 +19,7 @@ $title = 'Dragon Companion - The one and only Companion app for playing Dungeons
                 This is the one and only Dungeons and Dragons companion app to help you and your group play the game! It features all kinds of sweet features (some coming soon):
             </p>
             <ul>
-                <li>Timeline - this will help you with writing out your story in chucks split up in ways that I like to call the Circle-Arc-Segmeent methodology!</li>
+                <li>Timeline - this will help you with writing out your story in chucks split up in ways that I like to call the Circle-Arc-Segment methodology!</li>
                 <li>Character Creation <sup>(TBC)</sup> - create each of the characters from your group and track all of their stats and numbers as you play the game</li>
                 <li>Combat Tracker <sup>(TBC)</sup> - use your characters in the combat tracker to keep all of the bits of combat ticking over whilst you focus on the fun of battle!</li>
                 <li>Mini game mode <sup>(TBC)</sup> - as the name implies, making a mini game available so that you can play it in the same room or across the globe powered by Phaser!</li>
--- webroot/js/textarea-editor.js
+++ webroot/js/textarea-editor.js
@@ -1,18 +1,26 @@
 // var darkMode = console.log(getCookie('darkMode'));
 
 var mceConfig = {
+    // autoresize_bottom_margin: 15,
+    autoresize_bottom_margin: 200,
+    // autoresize_max_height: 200,
     branding: false,
+    browser_spellcheck: true,
     content_css: [
         '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
         '/css/mce.css?' + new Date().getTime()
     ],
+    force_br_newlines : true,
     formats: {
         underline: {inline : 'u'/*, 'classes' : 'underline'*/, exact : true},
         strikethrough: {inline : 'strike', exact: true},
         strike: {inline : 'strike', exact: true},
     },
+    invalid_elements : 'br',
+    invalid_children : 'br',
     menubar: false,
     mobile: {
+        content_css: [],
         theme: 'mobile',
         plugins: [
             'autosave', 'advlist',
@@ -20,22 +28,19 @@ var mceConfig = {
         toolbar: ['undo', 'redo', 'bold', 'italic', 'underline', 'strikethrough', 'bulllist', 'numlist', 'outdent', 'indent', 'autosave'],
     },
     plugins: [
-        'advlist autosave autolink lists link image charmap print preview anchor textcolor legacyoutput',
+        'advlist autosave autolink autoresize lists link image charmap print preview anchor textcolor legacyoutput',
         'searchreplace visualblocks code fullscreen',
         'insertdatetime media table contextmenu paste code help wordcount',
         'print preview searchreplace autolink directionality visualblocks visualchars template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern help'
     ],
     selector: '.textarea-editor-content',
     // toolbar: 'insert | undo redo | formatselect | bold italic underline strikethrough | table | bullist numlist outdent indent | removeformat | help',
-    toolbar: 'undo redo | bold italic underline strikethrough | bullist numlist | outdent indent | hr | table | removeformat | fullscreen | restoredraft | code',
-    force_br_newlines : true,
-    invalid_elements : 'br',
-    invalid_children : 'br',
+    toolbar: 'undo redo | bold italic underline strikethrough | bullist numlist | outdent indent | hr | table | removeformat | fullscreen autoresize | restoredraft | code',
 };
 
 if (getCookie('darkMode') == 1) {
     mceConfig.skin = 'charcoal';
-    let darkModeCss = '//localhost:8888/css/mce-dark.css?' + new Date().getTime();
+    let darkModeCss = '/css/mce-dark.css?' + new Date().getTime();
     mceConfig.content_css.push(
         darkModeCss
     );
