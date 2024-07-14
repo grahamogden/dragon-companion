71d985395024cca8e348143998ff3bf58ceb24e0
Graham Ogden <15658432+grahamogden@users.noreply.github.com>
2024-07-14 14:31:01 +0100

    Migrations - Added Timeline default permissions field to the Roles table

--- config/Migrations/20240702142000_CreateTimelines.php
+++ config/Migrations/20240702142000_CreateTimelines.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Model\Entity\Role;
+use App\Model\Table\RolesTable;
 use Migrations\AbstractMigration;
 
 /**
@@ -139,6 +141,16 @@ class CreateTimelines extends AbstractMigration
                 ['lft']
             )
             ->create();
+
+        $this->table(RolesTable::TABLE_NAME)
+            ->addColumn(Role::FIELD_TIMELINE_DEFAULT_PERMISSIONS, 'tinyinteger', [
+                'comment' => '[bitwise] deny:0,inherit:1,read:2,write:4,delete:8. For example, 6 means the role has read + write permissions but not delete. 10 Would mean read + delete but not write. 14 means that the role has read + write + delete permissions. Inherit must not be used for this value at all because this is the value that will be inherited from and will therefore be ignored.',
+                'default' => 0,
+                'limit' => 3,
+                'null' => false,
+                'signed' => false,
+            ])
+            ->update();
 
         /*************************
          *************************
