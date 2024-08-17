5491de94983f2b7baaf5cb61ff3d4cbe2dd17a7e
Graham Ogden <15658432+grahamogden@users.noreply.github.com>
2024-08-17 22:35:48 +0100

    UI - ContentGroup border colour change

--- src-js/ui/src/components/elements/ContentGroup.vue
+++ src-js/ui/src/components/elements/ContentGroup.vue
@@ -2,7 +2,7 @@
 <template>
     <div class="mb-6">
         <h3 v-if="$slots.heading"
-            class="mb-2 font-bold border-b-2 border-woodsmoke-700 dark:border-timberwolf-50 transition-colors duration-theme-change">
+            class="mb-2 font-bold border-b border-woodsmoke-700/50 dark:border-timberwolf-50/50 transition-colors duration-theme-change">
             <slot name="heading"></slot>
         </h3>
         <div>
