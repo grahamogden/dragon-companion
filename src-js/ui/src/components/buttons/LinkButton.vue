f5869b47a6a518113e771e264c5bf2e3150d4991
Graham Ogden <15658432+grahamogden@users.noreply.github.com>
2024-08-27 22:34:02 +0100

    UI - Fixed LinkButton width

--- src-js/ui/src/components/buttons/LinkButton.vue
+++ src-js/ui/src/components/buttons/LinkButton.vue
@@ -10,7 +10,7 @@
     <div class="inline-block bg-biscay-100 dark:bg-biscay-900 border border-biscay-600 rounded-lg overflow-hidden transition-colors duration-theme-change"
         :class="{ 'w-full': widthFull === true }">
         <router-link :to="props.destination"
-            class="py-2 px-4 hover:bg-biscay-600 focus:bg-biscay-600 text-biscay-800 dark:text-timberwolf-50 hover:text-timberwolf-50 focus:text-timberwolf-50 text-center no-underline duration-0">
+            class="w-full py-2 px-4 hover:bg-biscay-600 focus:bg-biscay-600 text-biscay-800 dark:text-timberwolf-50 hover:text-timberwolf-50 focus:text-timberwolf-50 text-center no-underline duration-0">
             <slot></slot>
         </router-link>
     </div>
