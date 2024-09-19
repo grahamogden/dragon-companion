ad0674b87e1617fb176c288de43954f6692dfe94
Graham Ogden <15658432+grahamogden@users.noreply.github.com>
2024-09-19 18:55:54 +0100

    Vite - Set host using env

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,35 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import autoprefixer from 'autoprefixer';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.ts',
-            refresh: true,
-            postcss: [
-                // tailwindcss(),
-                autoprefixer(),
-            ],
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig((mode) => {
+    const env = loadEnv(mode, process.cwd());
+    const host = `${env.VITE_HOST ?? ''}`;
+
+    return {
+        server: {
+            hmr: {
+                host: host
+            }
+        },
+        plugins: [
+            laravel({
+                input: 'resources/js/app.ts',
+                refresh: true,
+                postcss: [
+                    // tailwindcss(),
+                    autoprefixer(),
+                ],
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
-            },
-        }),
-    ],
+            }),
+        ],
+    }
 });
