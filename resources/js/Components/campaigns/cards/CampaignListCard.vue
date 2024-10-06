7e7f780f23a8fe876e2f9bd7885d3f19312a24c1
Graham Ogden <15658432+grahamogden@users.noreply.github.com>
2024-10-06 15:35:23 +0100

    UI Campaigns - Modified message when unselecting campaign

--- resources/js/Components/campaigns/cards/CampaignListCard.vue
+++ resources/js/Components/campaigns/cards/CampaignListCard.vue
@@ -4,7 +4,6 @@
   import type { CampaignEntity, CampaignEntityInterface } from '../../../types/entities/campaign';
   import ImageCard from '../../cards/ImageCard.vue'
   import KebabMenuItemButton from '../../menu/wrapped-kebab-menu/KebabMenuItemButton.vue';
-  import KebabMenuItemText from '../../menu/wrapped-kebab-menu/KebabMenuItemText.vue';
   import KebabMenuItemLink from '../../menu/wrapped-kebab-menu/KebabMenuItemLink.vue';
   import DropDownMenu from '../../drop-down/DropDownMenu.vue';
   import DropDownKebabIcon from '../../drop-down/DropDownKebabIcon.vue';
@@ -22,20 +21,19 @@
     notificationStore.removeAllNotifications()
     setTimeout(() => {
       campaignStore.selectCampaign(id, name)
-      notificationStore.addSuccess('Campaign ' + campaignStore.campaignName + ' successfully selected')
+      if (id === null) {
+        notificationStore.addSuccess('Campaign unselected')
+      } else {
+        notificationStore.addSuccess('Campaign "' + campaignStore.campaignName + '" successfully selected')
+      }
     }, 250)
   }
 
   const deleteCampaign = (campaign: CampaignEntity) => {
     console.debug('Confirming delete for ' + campaign.name)
     if (window.confirm('Are you sure you want to delete ' + campaign.name)) {
-      // notificationStore.removeAllNotifications()
       console.debug('Confirmed - attempting delete')
       router.delete(route('creator.campaigns.destroy', { campaign: campaign.id }))
-      // campaignStore.deleteCampaign(campaignId)
-      //   .then(() => {
-      //     notificationStore.addSuccess('Successfully deleted campaign')
-      //   })
     }
   }
 </script>
@@ -49,9 +47,13 @@
             <DropDownKebabIcon></DropDownKebabIcon>
           </template>
           <template #items>
-            <KebabMenuItemText v-if="campaign.id === campaignStore.selectedCampaignId">
+            <!-- <KebabMenuItemText v-if="campaign.id === campaignStore.selectedCampaignId">
               <font-awesome-icon :icon="['fas', 'ban']" fixed-width class="mr-2"></font-awesome-icon>Currently selected
-            </KebabMenuItemText>
+            </KebabMenuItemText> -->
+            <KebabMenuItemButton v-if="campaign.id === campaignStore.selectedCampaignId" :func="changeCampaign"
+              :args="[null, null]">
+              <font-awesome-icon :icon="['fas', 'ban']" fixed-width class="mr-2"></font-awesome-icon>Currently selected
+            </KebabMenuItemButton>
             <KebabMenuItemButton v-else :func="changeCampaign" :args="[campaign.id, campaign.name]">
               <font-awesome-icon :icon="['fas', 'check']" fixed-width class="mr-2"></font-awesome-icon>Select
             </KebabMenuItemButton>
