d33bfb8396002fc5090fac6fd678b02a5792cb3f
Graham Ogden <15658432+grahamogden@users.noreply.github.com>
2024-10-06 16:27:44 +0100

    UI - Added active state for nav links

--- resources/js/Components/NavLink/ToolbarNavLink.vue
+++ resources/js/Components/NavLink/ToolbarNavLink.vue
@@ -4,12 +4,15 @@
     const props = defineProps<{
         href: string
     }>()
+
+    const currentPage = new URL(props.href).pathname
 </script>
 
 <template>
     <Link
         class="hover:no-underline focus:no-underline focus:underline py-3 px-4 hover:lg:pl-8 focus:lg:pl-8 bg-transparent hover:bg-timberwolf-50 focus:bg-timberwolf-50 text-woodsmoke-950 dark:text-timberwolf-50 hover:text-shark-950 focus:text-shark-950 transition-all duration-300"
-        :href="props.href">
+        :class="{ 'active': $page.url.startsWith(currentPage) }" :href="props.href"
+        @click="($event) => { $event.target?.blur() }">
     <slot></slot>
     </Link>
 </template>
